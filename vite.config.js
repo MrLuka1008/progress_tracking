afa2fa734573b71670454e1b1abb49af10139861
Luka <lukadigmelashvili@gmail.com>
2025-03-21 20:54:42 +0400

    test

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/progress_tracking/",
+  base: "/",
   plugins: [react(), tailwindcss()],
 });
